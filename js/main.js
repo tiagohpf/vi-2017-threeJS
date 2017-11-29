cc2e0de8a0d6b02c52329a122f42e109b5e5710c
Tiago Faria <tiaghpf@ua.pt>
2017-11-29 00:05:49 +0000

    Changes on JS

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@
 
 var moving = true;
 var created = false;
-var raycaster = new THREE.Raycaster();
 var information;
 
-// --- SCENE CONFIGURATION ---
+// Configuração da cena
 var scene = new THREE.Scene();
 
 var width = window.innerWidth,
@@ -33,10 +32,7 @@ var radius = 1.4;
 var sphere = new THREE.Mesh(
     new THREE.SphereGeometry(radius, 32, 32),
     new THREE.MeshPhongMaterial({
-        map: texloader.load("images/earth_atmos_2048.jpg"),
-        bumpMap: texloader.load("images/earthbump.jpg"),
-        bumpScale: 0.05,
-        specularMap: texloader.load("images/earthspec.jpg"),
+        map: texloader.load("images/earth.jpg"),
         shading: THREE.SmoothShading
     })
 );
@@ -51,28 +47,6 @@ var clouds = new THREE.Mesh(
         depthWrite: false
     })
 );
-
-
-
-// --- EVENT LISTENERS ---
-
-function switch_presentation() {
-    for (var i in globe.children) {
-        if (globe.children[i].name != "") {
-            globe.children[i].visible = false;
-        }
-    }
-
-    for (var i in globe.children) {
-        for (var j in information) {
-            if (globe.children[i].sec_id == information[j].id && globe.children[i].name == document.getElementById("presentation_mode").value) {
-                globe.children[i].visible = true;
-            }
-        }
-    }
-    console.log(document.getElementById("presentation_mode").nodeValue);
-}
-
 
 
 window.addEventListener('resize', function () {
@@ -97,23 +71,18 @@ function onDocumentMouseDown(e) {
     var intersects = raycaster.intersectObjects(scene.children, true);
 
     if (intersects.length) {
-        // intersections are, by default, ordered by distance,
-        // so we only care for the first one. The intersection
-        // object holds the intersection point, the face that's
-        // been "hit" by the ray, and the object to which that
-        // face belongs. We only care for the object itself.
         var target = intersects[0].object;
-        if (target.name == 'pin' || target.name == 'bola') {
+        if (target.name == 'bola') {
             swal({
                 title: "Informações",
-                text: "Pescado em: " + target.data +
-                    " ás " + target.hora +
-                    "\nPelo Barco: " + target.sec_id +
-                    "\nNa região: " + target.latitude + "º " + target.longitude + "º" +
-                    "\n\nPeso do Pescado: " + target.pesoKg + " Kg" +
-                    "\nVelocidade: " + target.velocidade,
-                imageUrl: "images/fishing_icon.png",
+                text: "ID: " + target.sec_id +
+                    "\n\n Coordenadas: " + target.latitude + ", " + target.longitude +
+                    "\n\nData: " + target.data +
+                    "\n\nHora: " + target.hora +
+                    "\n\nPeso: " + target.pesoKg + " kg" +
+                    "\n\nVelocidade: " + target.velocidade,
                 confirmButtonText: "Fechar",
+                confirmButtonColor: '#0071bc',
                 allowOutsideClick: "true"
             });
         }
@@ -133,20 +102,8 @@ function onDocumentMouseMove(e) {
     var intersects = raycaster.intersectObjects(scene.children, true);
 
     if (intersects.length) {
-        // intersections are, by default, ordered by distance,
-        // so we only care for the first one. The intersection
-        // object holds the intersection point, the face that's
-        // been "hit" by the ray, and the object to which that
-        // face belongs. We only care for the object itself.
         var target = intersects[0].object;
-        if (target.name == 'pin') {
-            for (var i in globe.children) {
-                if (globe.children[i].name == 'pin') {
-                    globe.children[i].scale.set(0.1, 0.1, 0.2);
-                }
-            }
-            target.scale.set(0.2, 0.2, 0.4);
-        } else if (target.name == 'bola') {
+        if (target.name == 'bola') {
             for (var i in globe.children) {
                 if (globe.children[i].name == 'bola') {
                     globe.children[i].material.color.setHex(0xffff00);
@@ -156,9 +113,6 @@ function onDocumentMouseMove(e) {
         }
         else {
             for (var i in globe.children) {
-                if (globe.children[i].name == 'pin') {
-                    globe.children[i].scale.set(0.1, 0.1, 0.2);
-                }
                 if (globe.children[i].name == 'bola') {
                     globe.children[i].material.color.setHex(0xffff00);
                 }
@@ -174,17 +128,15 @@ var globe = new THREE.Object3D();
 globe.name = 'globe';
 globe.add(sphere);
 camera.position.z = 5;
-var pins = new THREE.Object3D();
 scene.add(globe);
-// -/- SCENE CONFIGURATION -/-
 
 
-// --- PIN FUNCTIONS ---
+// Create circles
 var render = function () {
     requestAnimationFrame(render);
     controls.update();
     if (moving) {
-        // a rotação não estava ao contrário?
+        // rotação
         globe.rotation.y += 0.00005;
     }
     renderer.render(scene, camera);
@@ -225,7 +177,7 @@ function createLocal(info) {
 
     //Esfera
     var geometry = new THREE.SphereBufferGeometry(Math.log10(info.weight) * .02, 32, 32);
-    var material = new THREE.MeshBasicMaterial({ color: 0xffff00, transparent: true, opacity: info.speed });
+    var material = new THREE.MeshBasicMaterial({color: 0xffff00, transparent: true, opacity: info.speed});
     var bola = new THREE.Mesh(geometry, material);
     bola.transparent = true
     bola.name = 'bola'
@@ -237,35 +189,10 @@ function createLocal(info) {
     bola.latitude = info.latitude;
     bola.longitude = info.longitude;
 
-    //pin
-    var pin = texloader.load('images/green_fish_marker.png');
-
-    var marker = new THREE.SpriteMaterial({
-        map: pin
-    });
-    var sprite = new THREE.Sprite(marker);
-    sprite.name = 'pin';
-
-    sprite.sec_id = info.id
-    sprite.pesoKg = info.weight;
-    sprite.velocidade = info.speed;
-    sprite.data = info.date;
-    sprite.hora = info.hour;
-    sprite.latitude = info.latitude;
-    sprite.longitude = info.longitude;
-
     var position = convertCoordinates(info.latitude.replace(",", "."), info.longitude.replace(",", "."));
-    var position2 = convertCoordinates2(info.latitude.replace(",", "."), info.longitude.replace(",", "."));
 
     //set esfera
     bola.position.set(position.x, position.y, position.z);
-
-    //set pin
-    sprite.position.set(position2.x, position2.y, position2.z);
-    sprite.scale.set(0.1, 0.1, 0.2);
-    globe.add(sprite);
-    sprite.visible = false;
-
 
     globe.add(bola);
 }
@@ -282,22 +209,8 @@ function convertCoordinates(lat, lon) {
     return new THREE.Vector3(x, y, z);
 }
 
-function convertCoordinates2(lat, lon) {
-
-    var phi = (90 - lat) * (Math.PI / 180);
-    var theta = (lon + 180) * (Math.PI / 180);
-
-    var x = -((radius + 0.05) * Math.sin(phi) * Math.cos(theta));
-    var z = ((radius + 0.05) * Math.sin(phi) * Math.sin(theta));
-    var y = ((radius + 0.05) * Math.cos(phi));
-
-    return new THREE.Vector3(x, y, z);
-}
-
-
-// --- EXECUTION ---
 $(window).load(function () {
-    $("canvas").css("visibility", "visible");
+    $("#canvas").css("visibility", "visible");
     $("#title").css("visibility", "visible");
     filterSearch()
 });
